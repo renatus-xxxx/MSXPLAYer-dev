a1bcab389762fe4f1fcd19dc859acc7078de35b9
nnkgw <148290899+renatus-xxxx@users.noreply.github.com>
2024-10-31 21:45:03 +0900

    add .sav file check

--- 01_update.bat
+++ 01_update.bat
@@ -1,13 +1,23 @@
 @echo off
 cd /d %~dp0
-set "SEARCH_PATH=C:\Program Files (x86)\ASCII\MSXマガジン永久保存版3\アスキー開発ツールズ(tR).exe"
-if exist "%SEARCH_PATH%" (
-  bin\file2sav\file2sav.exe "%userprofile%\AppData\Roaming\MSX Association\MSXPLAYer\アスキー開発ツールズ(tR).sav" @RESPONSE.TXT
-  taskkill /F /IM "アスキー開発ツールズ(tR).exe" >nul 2>&1
-  start "" "%SEARCH_PATH%"
-  taskkill /F /IM "savlist.exe" >nul 2>&1
-  start "" "bin\savlist\savlist.exe" "%userprofile%\AppData\Roaming\MSX Association\MSXPLAYer\アスキー開発ツールズ(tR).sav"
-  exit /b 0
+
+set "EXE_PATH=C:\Program Files (x86)\ASCII\MSXマガジン永久保存版3\アスキー開発ツールズ(tR).exe"
+set "SAV_PATH=%userprofile%\AppData\Roaming\MSX Association\MSXPLAYer\アスキー開発ツールズ(tR).sav"
+
+if exist "%EXE_PATH%" (
+  if exist "%SAV_PATH%" (
+    bin\file2sav\file2sav.exe "%userprofile%\AppData\Roaming\MSX Association\MSXPLAYer\アスキー開発ツールズ(tR).sav" @RESPONSE.TXT
+    taskkill /F /IM "アスキー開発ツールズ(tR).exe" >nul 2>&1
+    start "" "%EXE_PATH%"
+    taskkill /F /IM "savlist.exe" >nul 2>&1
+    start "" "bin\savlist\savlist.exe" "%userprofile%\AppData\Roaming\MSX Association\MSXPLAYer\アスキー開発ツールズ(tR).sav"
+    exit /b 0
+  ) else (
+    echo bat ファイルを書き換えて "アスキー開発ツールズ(tR).sav" へのフルパスを設定してください
+    echo (例:"%userprofile%\AppData\Roaming\MSX Association\MSXPLAYer\アスキー開発ツールズ(tR).sav")
+    pause
+    exit /b 1
+  )
 ) else (
   echo bat ファイルを書き換えて "アスキー開発ツールズ(tR).exe" へのフルパスを設定してください
   echo (例:"C:\Program Files (x86)\ASCII\MSXマガジン永久保存版3\アスキー開発ツールズ(tR).exe")
